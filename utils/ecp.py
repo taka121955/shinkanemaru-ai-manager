2f413c65354ebd62de3cd27c38144a0685472d44
taka121955 <ryuuseimaru55@gmail.com>
2025-06-12 23:46:59 +0900

    Update ecp.py

--- utils/ecp.py
+++ utils/ecp.py
@@ -1,15 +1,19 @@
-def reset_ecp():
-    return {
-        "loss_count": 0,
-        "base_bet": 100,
-    }
-
 def get_next_bet_amount(loss_count):
-    if loss_count == 0:
-        return 100
-    elif loss_count == 1:
-        return 300
-    elif loss_count == 2:
-        return 900
+    """
+    ECP方式による次回賭け金の決定。
+    """
+    base_bets = [100, 300, 900]
+    if loss_count < len(base_bets):
+        return base_bets[loss_count]
+    return 1300 * (2 ** (loss_count - 2))  # 2倍で増える想定
+
+
+def update_ecp(result):
+    """
+    勝敗に応じたECPカウント更新。
+    """
+    import streamlit as st
+    if result == "的中":
+        st.session_state.ecp["loss_count"] = 0
     else:
-        return 100
+        st.session_state.ecp["loss_count"] += 1
