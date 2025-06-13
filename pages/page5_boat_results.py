a0b9e39b2e87cb30f93e265add062d8e4267d550
taka121955 <ryuuseimaru55@gmail.com>
2025-06-13 18:29:11 +0900

    Update page5_boat_results.py

--- pages/page5_boat_results.py
+++ pages/page5_boat_results.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 def show():
     st.header("🚤 競艇結果ページ")
-    st.write("ここにレース結果などを表示する予定です。")
-    st.info("※このページは今後追加予定です。")
+    st.write("ここにレース結果を表示する予定です。")
+    st.info("※このページは現在開発中です。")
