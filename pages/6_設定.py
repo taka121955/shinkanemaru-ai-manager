c20ccc453ed47b84a0686ccd36e86d145b9341f8
taka121955 <ryuuseimaru55@gmail.com>
2025-06-17 23:58:44 +0900

    Update 6_設定.py

--- pages/6_設定.py
+++ pages/6_設定.py
@@ -2,5 +2,5 @@ import streamlit as st
 st.set_page_config(page_title="⑥ 設定", layout="centered")
 
 def show_page():
-    st.title("⚙️ アプリの設定")
-    st.info("設定機能は現在準備中です。")
+    st.title("⑥ 設定")
+    st.write("ユーザー設定をここで行います。")
