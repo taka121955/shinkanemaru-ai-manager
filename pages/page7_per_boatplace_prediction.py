55dadb07dcb429a08d02d37c89429b1d2c5f4c0d
taka121955 <ryuuseimaru55@gmail.com>
2025-06-18 00:13:54 +0900

    Update page7_per_boatplace_prediction.py

--- pages/page7_per_boatplace_prediction.py
+++ pages/page7_per_boatplace_prediction.py
@@ -3,4 +3,4 @@ st.set_page_config(page_title="⑦ 出走場別12R予想", layout="centered")
 
 def show_page():
     st.title("⑦ 出走場別12R予想")
-    st.write("各ボート場の予想をここに表示します。")
+    st.write("各競艇場の12R予想を表示します。")
