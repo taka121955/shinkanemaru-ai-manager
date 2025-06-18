5c41c575f2aebd088b80890aaa61573e40205a84
taka121955 <ryuuseimaru55@gmail.com>
2025-06-18 11:00:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pages.page1_ai_prediction import show_page as show_page1
 from pages.page2_input_result import show_page as show_page2
 from pages.page3_statistics import show_page as show_page3
 from pages.page4_record_result import show_page as show_page4
-from pages.page5_boat_results import show_page as show_page5
+from pages.page5_today_schedule import show_page as show_page5  # ← 正式名に修正
 from pages.page6_settings import show_page as show_page6
 from pages.page7_per_boatplace_prediction import show_page as show_page7
 from pages.page8_summary_today import show_page as show_page8
@@ -26,7 +26,7 @@ elif menu == "③ 統計データ":
 elif menu == "④ 結果履歴":
     show_page4()
 elif menu == "⑤ 出走表":
-    show_page5()
+    show_page5()  # ← ここも一致
 elif menu == "⑥ 設定":
     show_page6()
 elif menu == "⑦ 競艇場別予想":
